a67d2af01d27cfce0d94acae468b45ef00f523f7
bashir-7 <bshir12399@gmail.com>
2026-09-15 19:48:20 +0200

    Update chain_poops.js

--- g2all/slopkit/chain_poops.js
+++ g2all/slopkit/chain_poops.js
@@ -41,14 +41,14 @@ const STOP_BEFORE_DOUBLE = params.get("stop") === "beforedouble";
 function hostOk() {
     var m = document.getElementById("msgs");
     if (m) {
-        m.innerHTML = "GoldHEN v2.4b18.10 Loaded ...";
+        m.innerHTML = "تم تحميل GoldHEN بنجاح .تحياتي، مركز بشير";
     }
 }
 
 function hostFail() {
     var m = document.getElementById("msgs");
     if (m) {
-        m.innerHTML = "Failed to Load! Restart Your Console ...";
+        m.innerHTML = "فشل التحميل! أعد تشغيل جهازك .تحياتي، مركز بشير";
         m.style.color = "yellow";
     }
 }
@@ -455,7 +455,7 @@ let payloadRunning = false;
                 mark("ALREADY-ROOT", "getuid=" + uid0 + " setuid(0)=" + su0);
                 var m = document.getElementById("msgs");
                 if (m) {
-                    m.innerHTML = "GoldHEN is Already Loaded ...";
+                    m.innerHTML = "تم تحميل GoldHEN بالفعل .تحياتي، مركز بشير";
                 }
                 return;
             }
