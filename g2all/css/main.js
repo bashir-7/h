1aa8baf48d0719b2e35c3ef14dea8c9162b9093c
bashir-7 <bshir12399@gmail.com>
2026-09-15 19:51:01 +0200

    Update main.js

--- g2all/css/main.js
+++ g2all/css/main.js
@@ -53,7 +53,7 @@ async function doJb() {
     }
 
     if (fn.setuid.invoke(0) !== -1) {
-      msgs.innerHTML = "GoldHEN is Already Loaded ...";
+      msgs.innerHTML = "تم تحميل GoldHEN بالفعل .تحياتي، مركز بشير ";
       return;
     }
 
@@ -111,10 +111,10 @@ async function doJb() {
       load_bin(bin_u8);
     }
 
-    msgs.innerHTML = "GoldHEN v2.4b18.10 Loaded ...";
+    msgs.innerHTML = "تم تحميل GoldHEN بنجاح .تحياتي، مركز بشير";
     logger.info("===END===");
   } catch (e) {
-    msgs.innerHTML = "Failed to Load! Restart Your Console ...";
+    msgs.innerHTML = "فشل التحميل للأسف! أعد تشغيل جهازك .تحياتي، مركز بشير";
     msgs.style.color = "yellow";
   }
 }
