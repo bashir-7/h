d29210c1e4ea629408ff4aef18b7a6cc8aff59ba
bashir-7 <bshir12399@gmail.com>
2026-09-15 20:51:40 +0200

    Update chain_lapse.js

--- g2all/slopkit/chain_lapse.js
+++ g2all/slopkit/chain_lapse.js
@@ -36,7 +36,7 @@ const lines = [];
 function hostOk() {
     var m = document.getElementById("msgs");
     if (m) {
-        m.innerHTML = "تم تحميل GoldHEN .تحياتي، مركز بشير";
+        m.innerHTML = "تحياتي، مركز بشير GoldHEN تم تحميل";
     }
 }
 
